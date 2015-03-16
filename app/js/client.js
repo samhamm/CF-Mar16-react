54f7424f68e17c062eeb2aa898f28a10fbb3622f
samhamm <samhamm@gmail.com>
2015-03-16 07:49:27 -0700

    basic functionality in place

--- app/js/client.js
+++ app/js/client.js
@@ -3,20 +3,20 @@
 var React = require('react');
 var ajax = require('jquery').ajax;
 
-var celebsData = [{celebBody: 'hello world', _id: 1}, {celebBody: 'goodbye world', _id: 2}];
+var celebsData = [{handle: 'hello world', _id: 1}, {handle: 'goodbye world', _id: 2}];
 
 var Celeb = React.createClass({
   render: function() {
-    return <li>{this.props.data.celebBody}</li>
+    return <li>{this.props.data.handle}</li>
   }
 });
 
 var CelebForm = React.createClass({
   getInitialState: function() {
-    return {newCeleb: {celebBody: ''}};
+    return {newCeleb: {handle: ''}};
   },
   handleChange: function(event) {
-    this.setState({newCeleb: {celebBody: event.target.value}});
+    this.setState({newCeleb: {handle: event.target.value}});
   },
   handleSubmit: function(event) {
     event.preventDefault();
@@ -29,7 +29,7 @@ var CelebForm = React.createClass({
       data: JSON.stringify(newCeleb),
       success: function(data) {
         this.props.onNewCelebSubmit(data);
-        this.setState({newCeleb: {celebBody: ''}});
+        this.setState({newCeleb: {handle: ''}});
       }.bind(this),
       error: function(xhr, status, err) {
         console.log(err);
@@ -40,9 +40,25 @@ var CelebForm = React.createClass({
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="newceleb">New Celeb</label>
-        <input id="newceleb" type="text" value={this.state.newCeleb.celebBody} onChange={this.handleChange}/>
+        <input id="newceleb" type="text" value={this.state.newCeleb.handle} onChange={this.handleChange}/>
         <button type="submit">Create New Celeb</button>
       </form>
+    )
+  }
+});
+
+var CelebList = React.createClass({
+  render: function() {
+    var celebs = this.props.data.map(function(celeb) {
+      return <Celeb data={celeb} key={celeb._id}/>;
+    });
+    return (
+      <section>
+        <h1>Celebs:</h1>
+        <ul>
+          {celebs}
+        </ul>
+      </section>
     )
   }
 });
